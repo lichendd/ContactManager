bd2db4fa2b2097d192cb099ac879353d6d1e5e82
lichendd <47678173+lichendd@users.noreply.github.com>
2022-05-22 21:45:15 -0400

    backend almost done

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,22 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const config = require("config");
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/contacts
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+// @route GET api/contacts
 // @desc Get all users contacts
 // @access Private
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route  POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post("/", (req, res) => {
-  res.send("Add a new contact");
-});
+router.post(
+  "/",
+  [auth, [check("name", "Name is requried").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name, 
+        email,
+        phone, 
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route  PUT api/contacts
-// @desc update contact
+// @desc Update contact
 // @access Private
 router.put("/:id", (req, res) => {
   res.send("Update contact");
